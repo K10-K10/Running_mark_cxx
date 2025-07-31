85163083aaf4431c03afb407884fb539706a8e7a
K10-K10 <keito.tottori@gmail.com>
2025-07-31 23:49:23 +0900

    Edit: more crean

--- rotation/include/rotation_run_mark.cpp
+++ rotation/include/rotation_run_mark.cpp
@@ -22,14 +22,6 @@ void Rotation_run_mark::show_run_mark(bool flag_loop)
 		std::cout << "\x08" << "|" << std::flush;
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(Rotation_run_mark::time_delay_micro_seconds));
-		std::cout << "\x08" << "-" << std::flush;
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(Rotation_run_mark::time_delay_micro_seconds));
-		std::cout << "\x08" << "\\" << std::flush;
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(Rotation_run_mark::time_delay_micro_seconds));
-		std::cout << "\x08" << "|" << std::flush;
-		std::this_thread::sleep_for(std::chrono::milliseconds(Rotation_run_mark::time_delay_micro_seconds));
 		std::cout << "\x08";
 	}
 }
