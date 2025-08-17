8de2761c56293b4e2eee86ee99e51d47fcbc2bae
K10-K10 <keito.tottori@gmail.com>
2025-08-17 23:34:16 +0900

    Update directory

--- dot/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "dot_run_mark.hpp"
-
-int main()
-{
-	Dot_run_mark dot_run_mark;
-	bool flag = true;
-	dot_run_mark.show_run_mark(flag);
-	return 0;
-}
--- rotation/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "rotation_run_mark.hpp"
-
-int main()
-{
-	Rotation_run_mark rotation_run_mark;
-	bool flag = true;
-	rotation_run_mark.show_run_mark(flag);
-	return 0;
-}
